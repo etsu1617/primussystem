218325fd76d2e4c7e9cc1efeb08914b77bdb079c
pv-dn <e--yoshikata@outlook.jp>
2026-06-05 18:57:07 +0900

    PWA service worker追加

---
+++ client/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'purims-v1';
+const urlsToCache = [
+  '/primussystem/client/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
